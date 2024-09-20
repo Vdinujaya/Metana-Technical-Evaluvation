f4e793450aff746bad38d2b340d1da8a3bdc72d3
Vikum Dinujaya <vikumdinujaya@gmail.com>
2024-09-21 00:34:28 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container' style={{display:'flex', justifyContent:'center', alignItems:'center', height:'95vh'}}>
+        <iframe src='https://app.buildform.ai/forms/OYTSxFGq8GQc'
+    title="Embedded Form"
+    width="100%"
+    height="600px"
+    style={{border: 'none'}}
+    scrolling='no'
+  ></iframe>
+    </div>
+  )
+}
+
+export default Home
